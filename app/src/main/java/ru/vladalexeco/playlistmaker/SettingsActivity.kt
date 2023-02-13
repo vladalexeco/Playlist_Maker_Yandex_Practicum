c040b4778569c16fa425765c143e291cbb7e4139
vladalexeco <vladalexeco86@gmail.com>
2023-02-13 20:50:15 +0300

    added button click actions in SettingsActivity.kt

--- app/src/main/java/ru/vladalexeco/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/vladalexeco/playlistmaker/SettingsActivity.kt
@@ -1,16 +1,58 @@
 package ru.vladalexeco.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val backArrowImageView = findViewById<ImageView>(R.id.backArrowImageView)
+        val shareAppFrameLayout = findViewById<FrameLayout>(R.id.shareAppFrameLayout)
+        val supportFrameLayout = findViewById<FrameLayout>(R.id.supportFrameLayout)
+        val agreementFrameLayout = findViewById<FrameLayout>(R.id.agreementFrameLayout)
+
+
+        shareAppFrameLayout.setOnClickListener {
+            val message = "https://practicum.yandex.ru/android-developer/"
+
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, message)
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        supportFrameLayout.setOnClickListener {
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val mailArray = arrayOf("vladalexeco@yandex.ru")
+
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, mailArray)
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+        agreementFrameLayout.setOnClickListener {
+            val url = "https://yandex.ru/legal/practicum_offer/"
+
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(browserIntent)
+        }
 
         backArrowImageView.setOnClickListener {
             finish()
