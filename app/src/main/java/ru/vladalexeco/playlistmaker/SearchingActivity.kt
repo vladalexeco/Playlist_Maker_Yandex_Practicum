c15089baf37945bf869a516036ce8c3178228a6d
vladalexeco <vladalexeco86@gmail.com>
2023-03-16 20:19:50 +0300

    added retrofit actions

--- app/src/main/java/ru/vladalexeco/playlistmaker/SearchingActivity.kt
+++ app/src/main/java/ru/vladalexeco/playlistmaker/SearchingActivity.kt
@@ -5,38 +5,64 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchingActivity : AppCompatActivity() {
 
     var textFromSearchWidget = ""
+    private val baseUrl = "https://itunes.apple.com"
 
         companion object {
         const val EDIT_TEXT_VALUE = "EDIT_TEXT_VALUE"
+    }
+
+    val tracks = ArrayList<Track>()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val trackService = retrofit.create(ItunesApi::class.java)
+
+    private val adapter = TrackAdapter {
+        showInfo(it)
     }
 
     private lateinit var inputEditText: EditText
     private lateinit var clearButton: ImageView
     private lateinit var backArrowImageView: ImageView
     private lateinit var recyclerView: RecyclerView
+//    private lateinit var placeholderMessage: TextView
+    private lateinit var notFoundWidget: LinearLayout
+    private lateinit var badConnectionWidget: LinearLayout
+    private lateinit var updateButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-            val trackList = arrayListOf(
-        Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-        Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-        Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-        Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-        Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-    )
+//            val trackList = arrayListOf(
+//        Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
+//        Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
+//        Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
+//        Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
+//        Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
+//    )
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searching)
-
-        val adapter = TrackAdapter(trackList)
 
         recyclerView = findViewById(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -45,9 +71,19 @@ class SearchingActivity : AppCompatActivity() {
         inputEditText = findViewById(R.id.inputEditText)
         clearButton = findViewById(R.id.clearIcon)
         backArrowImageView = findViewById(R.id.backArrowImageView)
+//        placeholderMessage = findViewById(R.id.placeholderMessage)
+        notFoundWidget = findViewById(R.id.not_found_widget)
+        badConnectionWidget = findViewById(R.id.bad_connection_widget)
+        updateButton = findViewById(R.id.update_button)
+
+        adapter.tracks = tracks
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
+        }
+
+        updateButton.setOnClickListener {
+            search()
         }
 
         backArrowImageView.setOnClickListener {
@@ -70,8 +106,15 @@ class SearchingActivity : AppCompatActivity() {
         }
 
         inputEditText.addTextChangedListener(textWatcher)
-    }
 
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search()
+                true
+            }
+            false
+        }
+    }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -89,5 +132,72 @@ class SearchingActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    private fun showInfo(track: Track) {
+        Toast.makeText(applicationContext, "Track ${track.trackName} on ${tracks.indexOf(track)} position", Toast.LENGTH_SHORT).show()
+    }
+
+//    private fun showMessage(text: String, additionalMessage: String) {
+//        if (text.isNotEmpty()) {
+//            placeholderMessage.visibility = View.VISIBLE
+//            tracks.clear()
+//            adapter.notifyDataSetChanged()
+//            placeholderMessage.text = text
+//
+//            if (additionalMessage.isNotEmpty()) {
+//                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_SHORT).show()
+//            }
+//        } else {
+//            placeholderMessage.visibility = View.GONE
+//        }
+//    }
+
+    private fun showPlaceholder(flag: Boolean?) {
+        if (flag != null) {
+            if (flag == true) {
+                badConnectionWidget.visibility = View.GONE
+                notFoundWidget.visibility = View.VISIBLE
+            } else {
+                notFoundWidget.visibility = View.GONE
+                badConnectionWidget.visibility = View.VISIBLE
+            }
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+        } else {
+            notFoundWidget.visibility = View.GONE
+            badConnectionWidget.visibility = View.GONE
+        }
+    }
+
+    private fun search() {
+        trackService.search(text=inputEditText.text.toString())
+            .enqueue(object: Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    when(response.code()) {
+                        200 -> {
+                            if (response.body()?.tracks?.isNotEmpty() == true) {
+                                tracks.clear()
+                                tracks.addAll(response.body()?.tracks!!)
+                                adapter.notifyDataSetChanged()
+                                showPlaceholder(null)
+                            } else {
+                                showPlaceholder(true)
+                            }
+                        }
+                        else -> {
+                            showPlaceholder(false)
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    showPlaceholder(false)
+                }
+
+            })
     }
 }
--- app/src/main/java/ru/vladalexeco/playlistmaker/Track.kt
+++ app/src/main/java/ru/vladalexeco/playlistmaker/Track.kt
@@ -1,8 +1,10 @@
 package ru.vladalexeco.playlistmaker
 
+import com.google.gson.annotations.SerializedName
+
 data class Track(
     val trackName: String,
     val artistName: String,
-    val trackTime: String,
-    val artworkUrl: String
+    @SerializedName("trackTimeMillis") val trackTime: String,
+    @SerializedName("artworkUrl100") val artworkUrl: String
 )
