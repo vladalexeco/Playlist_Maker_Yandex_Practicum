efc62f4a2d3c271608fdbaee0737f8f710dd2fc0
vladalexeco <vladalexeco86@gmail.com>
2023-03-17 20:37:14 +0300

    preparation for submission to the review

--- app/src/main/java/ru/vladalexeco/playlistmaker/SearchingActivity.kt
+++ app/src/main/java/ru/vladalexeco/playlistmaker/SearchingActivity.kt
@@ -1,11 +1,14 @@
 package ru.vladalexeco.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -23,6 +26,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class SearchingActivity : AppCompatActivity() {
 
     var textFromSearchWidget = ""
+
     private val baseUrl = "https://itunes.apple.com"
 
         companion object {
@@ -46,23 +50,17 @@ class SearchingActivity : AppCompatActivity() {
     private lateinit var clearButton: ImageView
     private lateinit var backArrowImageView: ImageView
     private lateinit var recyclerView: RecyclerView
-//    private lateinit var placeholderMessage: TextView
     private lateinit var notFoundWidget: LinearLayout
     private lateinit var badConnectionWidget: LinearLayout
     private lateinit var updateButton: Button
+    private lateinit var badConnectionTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-//            val trackList = arrayListOf(
-//        Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-//        Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-//        Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-//        Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-//        Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-//    )
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searching)
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
         recyclerView = findViewById(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -71,15 +69,18 @@ class SearchingActivity : AppCompatActivity() {
         inputEditText = findViewById(R.id.inputEditText)
         clearButton = findViewById(R.id.clearIcon)
         backArrowImageView = findViewById(R.id.backArrowImageView)
-//        placeholderMessage = findViewById(R.id.placeholderMessage)
         notFoundWidget = findViewById(R.id.not_found_widget)
         badConnectionWidget = findViewById(R.id.bad_connection_widget)
         updateButton = findViewById(R.id.update_button)
+        badConnectionTextView = findViewById(R.id.bad_connection)
 
         adapter.tracks = tracks
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            inputMethodManager.hideSoftInputFromWindow(inputEditText.windowToken, 0)
         }
 
         updateButton.setOnClickListener {
@@ -138,22 +139,7 @@ class SearchingActivity : AppCompatActivity() {
         Toast.makeText(applicationContext, "Track ${track.trackName} on ${tracks.indexOf(track)} position", Toast.LENGTH_SHORT).show()
     }
 
-//    private fun showMessage(text: String, additionalMessage: String) {
-//        if (text.isNotEmpty()) {
-//            placeholderMessage.visibility = View.VISIBLE
-//            tracks.clear()
-//            adapter.notifyDataSetChanged()
-//            placeholderMessage.text = text
-//
-//            if (additionalMessage.isNotEmpty()) {
-//                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_SHORT).show()
-//            }
-//        } else {
-//            placeholderMessage.visibility = View.GONE
-//        }
-//    }
-
-    private fun showPlaceholder(flag: Boolean?) {
+    private fun showPlaceholder(flag: Boolean?, message: String = "") {
         if (flag != null) {
             if (flag == true) {
                 badConnectionWidget.visibility = View.GONE
@@ -161,6 +147,7 @@ class SearchingActivity : AppCompatActivity() {
             } else {
                 notFoundWidget.visibility = View.GONE
                 badConnectionWidget.visibility = View.VISIBLE
+                badConnectionTextView.text = message
             }
             tracks.clear()
             adapter.notifyDataSetChanged()
@@ -189,13 +176,13 @@ class SearchingActivity : AppCompatActivity() {
                             }
                         }
                         else -> {
-                            showPlaceholder(false)
+                            showPlaceholder(false, getString(R.string.server_error))
                         }
                     }
                 }
 
                 override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
-                    showPlaceholder(false)
+                    showPlaceholder(false, getString(R.string.bad_connection))
                 }
 
             })
